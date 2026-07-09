2131e1f72f1885a5b2118cad43f21d8f5eb9779b
Robert Plunk <blobertplunk@users.noreply.github.com>
2026-07-09 15:21:39 -0500

    Publish PlainOps storefront

---
+++ docs/assets/site-config.js
@@ -0,0 +1,8 @@
+window.PLAINOPS_CONFIG = {
+  productName: "Emergency Sub Plan Kit Grade 4",
+  price: "$7",
+  paymentUrl: "PAYMENT_LINK_PLACEHOLDER",
+  productZip: "assets/Emergency_Sub_Plan_Kit_Grade4_NoPrep.zip",
+  productSha256: "bfa862ddc094f5159f39a86f6e8d66b9e7c6fa27be6cd3c9d04aee47d3ad035b",
+  status: "Stage064 automated storefront builder"
+};
